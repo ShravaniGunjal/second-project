2702d6187dbae619c37166e340a790b368708e33
shravani.gunjal <shravani.gunjal05@gmail.com>
2025-08-19 16:19:19 +0530

    Program for calculator done successfully

--- src/App.js
+++ src/App.js
@@ -2,23 +2,34 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  let number1=10,number2=20,result=0;
+  function add(){
+    result=number1+number2;
+    console.log("The Addition is:"+result);
+  }
+  function subtract(){
+    result=number1-number2;
+    console.log("The Subtraction is:"+result);
+  }
+  function multiply(){
+    result=number1*number2;
+    console.log("The multiplication is:"+result);
+  }
+  function divide(){
+    result=number1/number2;
+    console.log("The Division is:"+result);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div>
+    <div>Calculator</div>
+    <div>The first number is 10</div>
+    <div>The second number is 20</div>
+    <div><button onClick={add}>Addition</button></div>
+    <div><button onClick={subtract}>Subtraction</button></div>
+    <div><button onClick={multiply}>Multiplication</button></div>
+    <div><button onClick={divide}>Division</button></div>
+   </div>
   );
 }
 
