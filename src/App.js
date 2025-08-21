a1f912e8e088cc1de53959b85abfdce7a46f3d96
shravani.gunjal <shravani.gunjal05@gmail.com>
2025-08-21 15:12:10 +0530

    multiplication table

--- src/App.js
+++ src/App.js
@@ -2,18 +2,21 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  let arr=[1,2,3,4,5,6];
+  let arr=[1,2,3,4,5,6,7,8,9,10];
+  let a=[2,3,4,5];
+  let multiplication=0;
   return (
     <div>{
-      arr.map((singleEle)=>{
-        if(singleEle%2===0){
-          return <i><div>{singleEle}</div></i>
-        }
-        else{
-          return <strong><div>{singleEle}</div></strong>
-        }
-      })
-}
+      a.map((singleElement)=>{
+        return <div>
+        {arr.map((Element)=>{
+          multiplication=singleElement*Element;
+          return(multiplication +"");
+        })}
+        
+        </div>
+      })}
+  
 </div>
     
 
