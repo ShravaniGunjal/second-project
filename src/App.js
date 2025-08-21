3040aa893b27a1367d265d01946fcff6e41a007d
shravani.gunjal <shravani.gunjal05@gmail.com>
2025-08-21 16:18:30 +0530

    accept number from user

--- src/App.js
+++ src/App.js
@@ -1,26 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  let arr=[1,2,3,4,5,6,7,8,9,10];
-  let a=[2,3,4,5];
-  let multiplication=0;
-  return (
-    <div>{
-      a.map((singleElement)=>{
-        return <div>
-        {arr.map((Element)=>{
-          multiplication=singleElement*Element;
-          return(multiplication +"");
-        })}
-        
-        </div>
-      })}
-  
-</div>
-    
+import { useState } from 'react';
 
+
+
+function App() {
+  const [age, setAge] = useState();
+  function handleAgeChange(event) {
+    setAge(event.target.value)
+  }
+  return (
+    <div>
+      <input name="age" value={age} onChange={handleAgeChange} />
+      <div>{age}</div>
+    </div>
   );
+
 }
 
 export default App;
