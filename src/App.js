fd3541d9549c272b5c1e886bc17b8cb7c64ca1b1
shravani.gunjal <shravani.gunjal05@gmail.com>
2025-08-22 15:25:06 +0530

    name with image

--- src/App.js
+++ src/App.js
@@ -6,14 +6,25 @@ import { useState } from 'react';
 
 
 function App() {
-  const [age, setAge] = useState();
-  function handleAgeChange(event) {
-    setAge(event.target.value)
+  const [name, setName] = useState();
+  const [showBlock, setShowBlock] = useState(false);
+  // const [image, setImage] = useState();
+
+  function handleNameChange(event) {
+    setName(event.target.value)
+  }
+  function OnSubmitClick() {
+    setShowBlock(true)
   }
   return (
     <div>
-      <input name="age" value={age} onChange={handleAgeChange} />
-      <div>{age}</div>
+      <div><input name="text" value={name} onChange={handleNameChange} /></div>
+      {/* <div>{name}</div> */}
+      <button onClick={OnSubmitClick}>Submit</button>
+     {showBlock ? <div>
+        <div>{name}</div>
+        <img src='https://4.imimg.com/data4/MM/YO/MY-9960839/flower-gardens.jpg' width='200px' />
+      </div>:<h1>Click on submit button</h1>}
     </div>
   );
 
