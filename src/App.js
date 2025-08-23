a1c2c04cb5dbdd05871670aa1d1ee3bf3d64b937
shravani.gunjal <shravani.gunjal05@gmail.com>
2025-08-23 12:24:00 +0530

    Calculation on click of buttons

--- src/App.js
+++ src/App.js
@@ -19,21 +19,45 @@ function App() {
   function handleNumber2Change(event) {
     setNumber2(parseInt(event.target.value))
   }
-  function onSubmit(){
+  function onAddition(){
     setAddition(number1+number2);
+    setSubtraction(undefined);
+    setMultiplication(undefined);
+    setDivision(undefined)
+    
+  }
+  function onSubtraction(){
     setSubtraction(number1-number2);
+    setAddition(undefined);
+    setMultiplication(undefined);
+    setDivision(undefined)
+    
+  }
+  function onMultiplication(){
     setMultiplication(number1*number2);
+    setAddition(undefined);
+    setSubtraction(undefined);
+    setDivision(undefined)
+  }
+  function onDivision(){
     setDivision(number1/number2)
+    setAddition(undefined);
+    setMultiplication(undefined);
+    setSubtraction(undefined);
   }
   return (
     <div>
       <div><input name='number' value={number1} placeholder='Enter First Number' onChange={handleNumber1Change}/></div>
       <input name='number' value={number2} placeholder='Enter Second Number' onChange={handleNumber2Change}/>
-      <div><button onClick={onSubmit}>Submit</button></div>
-      {addition &&<h1>Addition of {number1} and {number2} is : {addition}</h1>}
-      {subtraction &&<h1>Subtraction of {number1} and {number2} is : {subtraction}</h1>}
-      {multiplication &&<h1>Multiplication of {number1} and {number2} is : {multiplication}</h1>}
-      {division &&<h1>Division of {number1} and {number2} is : {division}</h1>}
+      <div>
+      <button onClick={onAddition}>Addition</button>
+      <button onClick={onSubtraction}>Subtraction</button>
+      <button onClick={onMultiplication}>Multiplication</button>
+      <button onClick={onDivision}>Division</button></div>
+      {addition !==undefined &&<h1>Addition of {number1} and {number2} is : {addition}</h1>}
+      {subtraction !==undefined &&<h1>Subtraction of {number1} and {number2} is : {subtraction}</h1>}
+      {multiplication !==undefined &&<h1>Multiplication of {number1} and {number2} is : {multiplication}</h1>}
+      {division !==undefined &&<h1>Division of {number1} and {number2} is : {division}</h1>}
     </div>
   );
 
